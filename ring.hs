b387e602e4773f060c103aff405d09bd21fdac2a
Said Kadrioski <said@kadrioski.de>
2023-12-16 14:25:55 +0100

    Simplify.

--- ring.hs
+++ ring.hs
@@ -7,20 +7,15 @@ import Debug.Trace
 --   you simply solve a linear equation.
 --
 ring :: (Integral a, Show a) => a -> a -> a
-ring a b = ring' aa (sig a * sig b)
+ring a b = ring' aa bb cc
   where
-    sig = signum
-    g   = gcd a b
-    aa  = ((abs a) `div` g) `mod` bb
-    bb  = (abs b) `div` g
+    aa  = (abs a) `mod` bb
+    bb  = abs b
+    cc  = signum a * signum b
 
-    -- Performance.
-    ring' a c
-      | bb `div` 2 < a = ring'' (bb-a) (-1 * c)
-      | otherwise = ring'' a c
-
-    -- The work.
-    ring'' 1 c = c
-    ring'' a c = trace ("We are at: " ++ (show a)) (ring'' (go a) (go' a c))
-    go a = a - (bb `mod` a)
-    go' a c = (c * (bb `div` a + 1)) `mod` bb
+    ring' 1 _ c = c
+    ring' a b c 
+      | b `div` 2 < a = ring' (b-a) b (-c)
+      | otherwise = trace ("Trace: " ++ (show a) ++ ", " ++ show(b) ++ ", " ++ show (c)) 
+      (ring' (b `mod` a) b (go a b c))
+    go a b c = (-c) * (b `div` a)
