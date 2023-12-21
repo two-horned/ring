e1540c225fa538431c583dc003d683225a049a4b
Said Kadrioski <said@kadrioski.de>
2023-12-21 22:37:35 +0100

    Finish algorithm.

--- ring.hs
+++ ring.hs
@@ -1,21 +1,12 @@
 module Ring where
-import Debug.Trace
--- | @'ring' @a @b returns you a valid s that solves
---   gcd(a,b) = sa + tb, where b is prime.
---   
---   If you want to find the corresponding value for t,
---   you simply solve a linear equation.
---
-ring :: (Integral a, Show a) => a -> a -> a
-ring a b = ring' aa bb cc
-  where
-    aa  = (abs a) `mod` bb
-    bb  = abs b
-    cc  = signum a * signum b
 
-    ring' 1 _ c = c
-    ring' a b c 
-      | b `div` 2 < a = ring' (b-a) b (-c)
-      | otherwise = trace ("Trace: " ++ (show a) ++ ", " ++ show(b) ++ ", " ++ show (c)) 
-      (ring' (b `mod` a) b (go a b c))
-    go a b c = (-c) * (b `div` a)
+-- | @'ring' @a @b returns you a valid s and t that solves
+--   gcd(a,b) = sa + tb.
+
+ring :: Integral a => a -> a -> (a,a)
+ring a 0 = (signum a, 0)
+ring 0 b = (0, signum b)
+ring a b = ((t * (d+1) - s*d),(s-t))
+  where
+    (d,c) = b `divMod` a
+    (s,t) = ring c (a-c)
