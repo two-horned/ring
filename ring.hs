8fd703235d343d06a245072f47e523691561f443
Said Kadrioski <said@kadrioski.de>
2023-12-21 23:40:44 +0100

    Add own gcd.

--- ring.hs
+++ ring.hs
@@ -1,8 +1,15 @@
 module Ring where
 
+-- | @'gcd' @a @b returns you the greatest 
+--   common divisor of two numbers.
+gcd a 0 = abs a
+gcd 0 b = abs b
+gcd a b = Ring.gcd c (a-c)
+  where 
+    c = b `mod` a
+
 -- | @'ring' @a @b returns you a valid s and t that solves
 --   gcd(a,b) = sa + tb.
-
 ring :: Integral a => a -> a -> (a,a)
 ring a 0 = (signum a, 0)
 ring 0 b = (0, signum b)
