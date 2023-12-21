23d16374307fb167d4fd75003633b3c7da68d7d0
Said Kadrioski <said@kadrioski.de>
2023-12-22 00:00:55 +0100

    Prettify?

--- ring.hs
+++ ring.hs
@@ -1,14 +1,33 @@
 module Ring where
 
+import Prelude hiding (gcd, lcm)
+
+
+-- We will first introduce our own implementations
+-- of the gcd and the lcd to make it easier for the reader 
+-- to understand why our ring function is built the way it is.
+
 -- | @'gcd' @a @b returns the greatest 
 --   common divisor of two numbers.
 gcd a 0 = abs a
 gcd 0 b = abs b
-gcd a b = Ring.gcd c (a-c)
+gcd a b = gcd c (a-c)
   where 
     c = b `mod` a
 
--- | @'ring' @a @b returns a valid s and t that solves
+-- | @'lcm' @a @b returns the least
+--   common multiple of two numbers.
+lcm a 0 = abs a
+lcm 0 b = abs b
+lcm a b = (a `div` gcd a b) * b
+
+
+-- For the ring function, 
+-- we don't actually need the gcd or lcm functions 
+-- from before. 
+-- They were only there for demonstrational purposes.
+
+-- | @'ring' @a @b returns a valid s and t that solve
 --   gcd(a,b) = sa + tb.
 ring :: Integral a => a -> a -> (a,a)
 ring a 0 = (signum a, 0)
@@ -17,3 +36,5 @@ ring a b = ((t * (d+1) - s*d),(s-t))
   where
     (d,c) = b `divMod` a
     (s,t) = ring c (a-c)
+
+
