510d78afa72460c0e59772eba20d8676b7e19e1e
Said Kadrioski <said@kadrioski.de>
2023-12-22 22:26:39 +0100

    Performance changes.

--- ring.hs
+++ ring.hs
@@ -3,18 +3,19 @@ module Ring where
 import Prelude hiding (gcd, lcm)
 
 -- We will first introduce our own implementations
--- of the gcd and the lcd to make it easier for the reader 
+-- of the gcd and the lcm to make it easier for the reader 
 -- to understand why our ring function is built the way it is.
 
 -- | @'gcd' @a @b returns the greatest 
 --   common divisor of two numbers.
-gcd a 0 = abs a
-gcd 0 b = abs b
-gcd a b 
-  | abs b < abs a = gcd b a
-  | otherwise = gcd (a - c) c
+gcd a b = gcd' (abs a) (abs b)
   where
-    c = b `mod` a
+    gcd' 0 b = b
+    gcd' a b 
+      | b < a = gcd' b a
+      | otherwise = gcd' (a - c) c
+      where 
+        c = b `rem` a
 
 -- | @'lcm' @a @b returns the least
 --   common multiple of two numbers.
@@ -31,12 +32,13 @@ lcm a b = (a `div` gcd a b) * b
 -- | @'egcd' @a @b returns a valid s and t that solve
 --   gcd(a,b) = sa + tb. Meaning it's the extended gcd.
 egcd :: Integral a => a -> a -> (a,a)
-egcd a 0 = (signum a, 0)
-egcd 0 b = (0, signum b)
-egcd a b
-  | abs b < abs a = (ss, tt)
-  | otherwise = (((s-t)* d + s),(t-s))
+egcd a b = egcd' (abs a) (abs b)
   where
-    (d,c) = b `divMod` a
-    (s,t) = egcd (a - c) c
-    (tt, ss) = egcd b a
+    egcd' 0 b = (0, signum b)
+    egcd' a b 
+      | b < a = (ss,tt)
+      | otherwise = (((s-t)* d + s),(t-s))
+      where
+        (d,c) = b `quotRem` a
+        (s,t) = egcd' (a - c) c
+        (tt,ss) = egcd' b a
