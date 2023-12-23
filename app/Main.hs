8291071dc1369c8cad6fbd195103b6b48f899f9e
Said Kadrioski <said@kadrioski.de>
2023-12-23 02:14:03 +0100

    Switch to cabal.

---
+++ app/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import qualified Ring (egcd)
+
+fib :: Integer -> Integer
+fib n 
+  | n < 0 = 0
+  | otherwise = go n 0 1
+  where
+    go 0 _ b = b
+    go m a b = go (m-1) b (a+b)
+
+main :: IO ()
+main = do
+  putStrLn ("EGCD of " ++ (show a) ++ " and " ++ (show b) ++ " is " ++ show (Ring.egcd a b))
+  where 
+    a = fib 100 
+    b = fib 101
