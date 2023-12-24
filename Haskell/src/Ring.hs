d79b1356e97fe2065e902c514cf15b2ebe46bb98
Said Kadrioski <said@kadrioski.de>
2023-12-24 01:40:30 +0100

    Remove chunk comments.

--- Haskell/src/Ring.hs
+++ Haskell/src/Ring.hs
@@ -2,10 +2,6 @@ module Ring where
 
 import Prelude hiding (gcd, lcm)
 
--- We will first introduce our own implementations
--- of the gcd and the lcm to make it easier for the reader 
--- to understand why our ring function is built the way it is.
-
 -- | @'gcd' @a @b returns the greatest 
 --   common divisor of two numbers.
 gcd :: Integral a => a -> a -> a
@@ -25,11 +21,6 @@ lcm a 0 = abs a
 lcm 0 b = abs b
 lcm a b = (a `quot` gcd a b) * b
 
-
--- For the egcd function, 
--- we don't actually need the gcd or lcm functions 
--- from before. 
--- They were only there for demonstrational purposes.
 
 -- | @'egcd' @a @b returns a valid s and t that solve
 --   gcd(a,b) = sa + tb. Meaning it's the extended gcd.
