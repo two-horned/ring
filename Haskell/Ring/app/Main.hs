c32e246b0c0275e0d14735884822039bcba35762
Said Kadrioski <said@kadrioski.de>
2023-12-27 02:36:08 +0100

    Delete unused function.

--- Haskell/Ring/app/Main.hs
+++ Haskell/Ring/app/Main.hs
@@ -1,25 +1,6 @@
 module Main where
 
 import qualified Ring (egcd)
-import Control.Monad.ST
-import Data.STRef
-
-fibST :: Integer -> Integer
-fibST n = 
-    if n < 2
-    then n
-    else runST $ do
-        x <- newSTRef 0
-        y <- newSTRef 1
-        fibST' n x y
-
-    where fibST' 0 x _ = readSTRef x
-          fibST' n x y = do
-              x' <- readSTRef x
-              y' <- readSTRef y
-              writeSTRef x y'
-              writeSTRef y $! x'+y'
-              fibST' (n-1) x y
 
 fib :: Integer -> Integer
 fib n 
